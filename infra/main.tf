96f478f6519db9699ca7e9240061fc66e1137236
botvesga <fvesga25@gmail.com>
2025-08-05 13:20:36 -0400

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -13,22 +13,38 @@ provider "aws" {
   region = var.aws_region
 }
 
-# 1) VPC por defecto
+# 1) Traemos la VPC por defecto
 data "aws_vpc" "default" {
   default = true
 }
 
-# 2) DB Subnet Group ya creado
+# 2) Traemos el Security Group "default" explícitamente
+data "aws_security_group" "default_sg" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+
+  # Necesitamos el nombre del SG:
+  # Terraform >=0.13 lo infiere, si falla indica:
+  #   vpc_id = data.aws_vpc.default.id
+}
+
+# 3) Traemos el DB Subnet Group existente
 data "aws_db_subnet_group" "existing" {
   name = "scalping-db-subnet-group"
 }
 
-# 3) ECS Cluster
+# 4) Creamos el ECS Cluster
 resource "aws_ecs_cluster" "scalping" {
   name = "scalping-cluster"
 }
 
-# 4) RDS TimescaleDB
+# 5) Creamos la RDS (TimescaleDB)
 resource "aws_db_instance" "timescaledb" {
   identifier           = "scalping-db"
   engine               = "postgres"
@@ -39,9 +55,9 @@ resource "aws_db_instance" "timescaledb" {
   password             = var.db_password
   publicly_accessible  = false
 
-  # <-- aquí usamos el default SG de la VPC
+  # <-- aquí apuntamos al SG default que acabamos de capturar
   vpc_security_group_ids = [
-    data.aws_vpc.default.default_security_group_id
+    data.aws_security_group.default_sg.id
   ]
 
   db_subnet_group_name = data.aws_db_subnet_group.existing.name
